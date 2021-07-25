d9bd93e91e552dcc0ad12b9902de148638ebaa7f
derfelot <derfelot.git@gmail.com>
2021-07-25 18:15:54 +0200

    sm8350-common: rootdir: Cleanup of Anroid.mk

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -19,12 +19,12 @@ LOCAL_MODULE_PATH      := $(TARGET_VENDOR_RAMDISK_OUT)/first_stage_ramdisk
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.recovery.qcom.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.recovery.qcom.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE           := init.recovery.qcom.rc
+LOCAL_MODULE_TAGS      := optional
+LOCAL_MODULE_TAGS      := optional
+LOCAL_MODULE_CLASS     := ETC
+LOCAL_SRC_FILES        := etc/init.recovery.qcom.rc
+LOCAL_MODULE_PATH      := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
