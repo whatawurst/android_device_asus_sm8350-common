a2f46ddf7630c77b8fd4465529188deb3004e4c3
derfelot <derfelot.git@gmail.com>
2021-08-16 02:46:12 +0200

    sm8350-common: Build vndfwk_detect from source

--- platform/product_packages.mk
+++ platform/product_packages.mk
@@ -174,6 +174,14 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.perf@2.1.vendor \
     vendor.qti.hardware.perf@2.2.vendor
 
+# QMI
+PRODUCT_PACKAGES += \
+    libjson \
+    libqti_vndfwk_detect \
+    libqti_vndfwk_detect.vendor \
+    libvndfwk_detect_jni.qti \
+    libvndfwk_detect_jni.qti.vendor
+
 # QTI service tracker
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.servicetracker@1.0.vendor \
