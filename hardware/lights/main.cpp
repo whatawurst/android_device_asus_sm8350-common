70c41d244fd386702f78dffe34f61d83a97d53b5
Andreas Schneider <asn@cryptomilk.org>
2021-08-24 13:49:57 +0200

    hardware: Import QTI lights HAL which loads lights.qcom
    https://source.codeaurora.org/quic/la/device/qcom/vendor-common/tree/lights?h=LA.UM.9.14.r1-17900-LAHAINA.0
    
    Change-Id: Ia318d27f140f501bcb2842c603680c304a0f5fd9

--- hardware/lights/main.cpp
+++ hardware/lights/main.cpp
@@ -15,7 +15,6 @@
  */
 
 #include "Lights.h"
-
 #include <android-base/logging.h>
 #include <android/binder_manager.h>
 #include <android/binder_process.h>
@@ -25,6 +24,9 @@ using ::aidl::android::hardware::light::Lights;
 int main() {
     ABinderProcess_setThreadPoolMaxThreadCount(0);
     std::shared_ptr<Lights> lights = ndk::SharedRefBase::make<Lights>();
+    if (!lights) {
+        return EXIT_FAILURE;
+    }
 
     const std::string instance = std::string() + Lights::descriptor + "/default";
     binder_status_t status = AServiceManager_addService(lights->asBinder().get(), instance.c_str());
