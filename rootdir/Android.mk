e7ef4a0adb616e4406b05a3765fffc278883371b
derfelot <derfelot.git@gmail.com>
2021-08-26 01:16:06 +0200

    sm8350-common: init: Drop Asus debugtool
    Also drop
      * Associated selinux policies
      * Rootdir creation
      * Entry in init.sus.rc

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -35,14 +35,6 @@ LOCAL_MODULE_PATH      := $(TARGET_VENDOR_RAMDISK_OUT)/first_stage_ramdisk
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE           := init.asus.debugtool.rc
-LOCAL_MODULE_TAGS      := optional
-LOCAL_MODULE_CLASS     := ETC
-LOCAL_SRC_FILES        := etc/init.asus.debugtool.rc
-LOCAL_MODULE_PATH      := $(TARGET_OUT_VENDOR_ETC)/init/hw
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE           := init.asus.rc
 LOCAL_MODULE_TAGS      := optional
 LOCAL_MODULE_CLASS     := ETC
